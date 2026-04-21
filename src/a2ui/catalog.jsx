2ffe54444b6d85020007016c9d07f56025fb6110
Aditya Singh <adi.singh199@gmail.com>
2026-04-20 18:02:36 -0700

    Fixed build issue

--- src/a2ui/catalog.jsx
+++ src/a2ui/catalog.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import React from 'react';
 import { Card, CardContent } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
